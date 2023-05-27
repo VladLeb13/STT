2c42a09cabf0eef7b7489029882ba1bb945405d3
lebedev <vlad194127@gmail.com>
2023-05-27 16:08:08 +0500

    подключил палетку

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,13 @@
 </template>
 
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $dark-blue;
 }
 
 nav {
@@ -20,10 +21,10 @@ nav {
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $lighter-blue;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $dark-blue;
     }
   }
 }
